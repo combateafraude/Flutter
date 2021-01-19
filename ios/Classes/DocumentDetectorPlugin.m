e818f43b144fbb75a30d48abfdb5a0f737b8f581
Daniel <daniel.seitenfus@hotmail.com>
2021-01-19 17:17:00 -0300

    fix: rename podspec file

--- ios/Classes/DocumentDetectorPlugin.m
+++ ios/Classes/DocumentDetectorPlugin.m
@@ -1,11 +1,11 @@
 #import "DocumentDetectorPlugin.h"
-#if __has_include(<document_detector/document_detector-Swift.h>)
-#import <document_detector/document_detector-Swift.h>
+#if __has_include(<document_detector_nodatabinding/document_detector_nodatabinding-Swift.h>)
+#import <document_detector_nodatabinding/document_detector_nodatabinding-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "document_detector-Swift.h"
+#import "document_detector_nodatabinding-Swift.h"
 #endif
 
 @implementation DocumentDetectorPlugin
