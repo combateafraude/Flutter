887c23801c8872cabbb07e4daa09e92256d64195
Daniel <daniel.seitenfus@hotmail.com>
2021-04-01 14:15:45 -0300

    Revert "feat: upload diles"
    This reverts commit d612c6a0b4ab420394219b8b342d55f04bb05c05.

--- example/ios/Pods/Target Support Files/address_check/address_check-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_address_check : NSObject
-@end
-@implementation PodsDummy_address_check
-@end
--- example/ios/Pods/TensorFlowLiteC/Frameworks/TensorFlowLiteC.framework/Headers/TensorFlowLiteC.h
+++
@@ -1,3 +0,0 @@
-#import "metal_delegate.h"
-#import "c_api.h"
-#import "c_api_types.h"
