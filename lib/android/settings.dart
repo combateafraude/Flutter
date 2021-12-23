adf179c12e4e5285e18a75e307bd81ebf6bf5405
Daniel Seitenfus <daniel.seitenfus@hotmail.com>
2021-12-23 13:35:02 -0300

    Add face-authenticator-compatible-v3.5.0

--- lib/android/settings.dart
+++ lib/android/settings.dart
@@ -6,13 +6,18 @@ class FaceAuthenticatorAndroidSettings {
 
   SensorSettingsAndroid sensorSettings;
 
-  FaceAuthenticatorAndroidSettings({this.customization, this.sensorSettings});
+  bool enableEmulator;
+  bool enableRootDevices;
+
+  FaceAuthenticatorAndroidSettings({this.customization, this.sensorSettings, this.enableEmulator, this.enableRootDevices});
 
   Map asMap() {
     Map<String, dynamic> map = new Map();
 
     map["customization"] = customization?.asMap();
     map["sensorSettings"] = sensorSettings?.asMap();
+    map["enableEmulator"] = enableEmulator;
+    map["enableRootDevices"] = enableRootDevices;
 
     return map;
   }
